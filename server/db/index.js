6cada47b7adc15dd50972cb689c3a38e1cab381f
root <root@James>
2023-01-09 16:32:17 -0500

    added relations | need add models

--- server/db/index.js
+++ server/db/index.js
@@ -4,13 +4,17 @@ const db = require('./db')
 
 const User = require('./models/User')
 const Product = require ('./models/Product')
+const Cart = require ('./models/Cart')
 
-User.hasMany(Product)
+Cart.belongsTo(User)
+Cart.hasMany(Product)
 
 module.exports = {
   db,
   models: {
     User,
     Product,
+    Cart
   },
 }
+
