eabc7249defd132dda38a0d17f87cdf55ece090d
Mike <mgryan189@gmail.com>
2023-01-11 12:57:50 -0500

    all products updates

--- client/features/allproducts/AddProduct.js
+++ client/features/allproducts/AddProduct.js
@@ -22,26 +22,31 @@ const AddProduct = () => {
       <label htmlFor="imageUrl">Image Url:</label>
       <input
         name="imageUrl"
+        value={imageUrl}
         onChange={(e) => setImageUrl(e.target.value)}
       />
       <label htmlFor="name">Name:</label>
       <input
         name="name"
+        value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <label htmlFor="price">Price:</label>
       <input
         name="Price"
+        value={price}
         onChange={(e) => setPrice(e.target.value)}
       />
       <label htmlFor="description">Description:</label>
       <input
         name="Description"
+        value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
       <label htmlFor="quantity">Quantity:</label>
       <input
         name="Quantity"
+        value={quantity}
         onChange={(e) => setQuantity(e.target.value)}
       />
       <button type="submit">Add Product</button>
